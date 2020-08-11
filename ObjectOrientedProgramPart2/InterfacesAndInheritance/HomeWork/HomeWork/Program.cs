25e5ebbf4429845530d972503972c3a022d06d79
Дианов Е.Д <dianov@so-line.ru>
2020-08-11 08:54:08 +0300

    InterfaceAndInheritance

---
+++ ObjectOrientedProgramPart2/InterfacesAndInheritance/HomeWork/HomeWork/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IRunnable> runnable = new List<IRunnable>();
+
+            People people = new People();
+
+            Animal animal = new Animal();
+
+            runnable.Add(people);
+            runnable.Add(animal);
+
+            foreach(IRunnable rb in runnable)
+            {
+                rb.Run();
+
+                if(rb is People ppl)
+                {
+                    ppl.Read();
+                }
+
+                if(rb is Animal an)
+                {
+                    an.DrinkWater();
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+
+        
+    }
+
+    public class People: IRunnable
+    {
+        public void Read()
+        {
+            Console.WriteLine("People read");
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("People run");
+        }
+    }
+
+    public class Animal: IRunnable
+    {
+        public void  DrinkWater()
+        {
+            Console.WriteLine("Animal drink water");
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("Animal run");
+        }
+    }
+
+    public interface IRunnable
+    {
+        void Run();
+    }
+
+}
---
+++ ObjectOrientedProgramPart2/InterfacesAndInheritance/InheritanceMiniProject/InheritanceMiniProject/InheritanceMiniProject/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceMiniProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<InventoryItemModel> inventory = new List<InventoryItemModel>();
+
+            inventory.Add(new VehicleModel { DealerFee = 25, ProductName = "Kia Optima" });
+            inventory.Add(new BookModel { ProductName = "A Tale of Two Cities", NumberOfPages = 200 });
+
+            List<IRentable> rentables = new List<IRentable>();
+            List<IPurchasable> purchasable = new List<IPurchasable>();
+
+            var vehicle = new VehicleModel { DealerFee = 50, ProductName = "Polo Sedan", QuantityInStock = 100 };
+            var book = new BookModel { ProductName = "Fellowship of the ring" };
+            var excavator = new ExcavatorModel { ProductName = "Buldozer", QuantityInStock = 2 };
+
+            Console.ReadLine();
+        }
+    }
+
+    public interface IRentable
+    {
+        void Rent();
+        void ReturnRental();
+    }
+
+    public interface IPurchasable
+    {
+        void Purchase();
+    }
+
+    public class InventoryItemModel
+    {
+        public string ProductName { get; set; }
+        public int QuantityInStock { get; set; }
+    }
+
+    public class VehicleModel: InventoryItemModel, IPurchasable, IRentable
+    {
+        public decimal DealerFee { get; set; }
+
+        public void Purchase()
+        {
+            QuantityInStock -= 1;
+            Console.WriteLine("This vehicle has been purchased");
+        }
+
+        public void Rent()
+        {
+            QuantityInStock -= 1;
+            Console.WriteLine("This vehicle has been rented");
+        }
+
+        public void ReturnRental()
+        {
+            QuantityInStock += 1;
+            Console.WriteLine("This vehicle has been returned");
+        }
+    }
+
+    public class BookModel : InventoryItemModel, IPurchasable
+    {
+        public int NumberOfPages { get; set; }
+
+        public void Purchase()
+        {
+            QuantityInStock -= 1;
+            Console.WriteLine("This book has been purchased");
+        }
+    }
+
+    public class ExcavatorModel: InventoryItemModel, IRentable
+    {
+        public void Dig()
+        {
+            Console.WriteLine("I am digging");
+        }
+
+        public void Rent()
+        {
+            QuantityInStock -= 1;
+            Console.WriteLine("This excavator has been rented");
+        }
+
+        public void ReturnRental()
+        {
+            QuantityInStock += 1;
+            Console.WriteLine("The excavator has been returned");
+        }
+    }
+
+
+}
