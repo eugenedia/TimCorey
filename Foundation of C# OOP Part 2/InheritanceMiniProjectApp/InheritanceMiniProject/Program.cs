26612183197acd9338af7e485d94d7d01767917b
Eugene Dianov <Eugenedia@gmail.com>
2021-01-29 17:08:13 +0300

    Доработка проекта

---
+++ Foundation of C# OOP Part 2/InheritanceMiniProjectApp/InheritanceMiniProject/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceMiniProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IRentable> rentables = new List<IRentable>();
+            List<IPurchasable> purchasables = new List<IPurchasable>();
+
+            var vehicle = new VehicleModel { DealerFee = 25, ProductName = "Kia Optima" };
+            var book = new BookModel { ProductName = "A Tale of Two Cities", NumberOfPages = 350 };
+            var excavaror = new ExcavatorModel { ProductName = "Bulldozer", QuantityInStock = 2 };
+
+            rentables.Add(vehicle);
+            rentables.Add(excavaror);
+
+
+            purchasables.Add(book);
+            purchasables.Add(vehicle);
+
+            Console.Write("Do you want to rent or purchase something: (rent, purchase) ");
+            string rentalDecision = Console.ReadLine();
+
+            if (rentalDecision.ToLower() == "rent")
+            {
+                foreach(var item in rentables)
+                {
+                    Console.WriteLine($"Item: {item.ProductName}");
+                    Console.Write("Do you want to rent this item (yes/no): ");
+                    
+                    string wantToRent = Console.ReadLine();
+
+                    if (wantToRent.ToLower() == "yes")
+                    {
+                        item.Rent();
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+    
+    public interface IInventoryItem
+    {
+        string ProductName { get; set; }
+        int QuantityInStock { get; set; }
+    }
+
+    public interface IRentable : IInventoryItem
+    {
+        void Rent();
+        void ReturnRental();
+    }
+
+    public interface IPurchasable : IInventoryItem
+    {
+        void Purchase();
+    }
+
+    public class InventoryItemModel : IInventoryItem
+    {
+        public string ProductName { get; set; }
+        public int QuantityInStock { get; set; }
+    }
+
+    public class VehicleModel : InventoryItemModel, IPurchasable, IRentable
+    {
+        public decimal DealerFee { get; set; }
+
+        public void Purchase()
+        {
+            QuantityInStock -= 1;
+            Console.WriteLine("This vehicle has been purchased");
+        }
+
+        public void Rent()
+        {
+            QuantityInStock -= 1;
+            Console.WriteLine("This vehicle has been rented");
+        }
+
+        public void ReturnRental()
+        {
+            QuantityInStock += 1;
+            Console.WriteLine("This vehicle has been returned");
+        }
+    }
+
+    public class BookModel : InventoryItemModel, IPurchasable
+    {
+        public int NumberOfPages { get; set; }
+
+        public void Purchase()
+        {
+            QuantityInStock -= 1;
+            Console.WriteLine("This book has been purchased.");
+        }
+    }
+
+    public class ExcavatorModel : InventoryItemModel, IRentable
+    {
+        public void Dig()
+        {
+            Console.WriteLine("I am digging");
+        }
+
+        public void Rent()
+        {
+            QuantityInStock -= 1;
+            Console.WriteLine("This excavator has benn rented");
+        }
+
+        public void ReturnRental()
+        {
+            QuantityInStock += 1;
+            Console.WriteLine("The excavator has been returned");
+        }
+    }
+}
